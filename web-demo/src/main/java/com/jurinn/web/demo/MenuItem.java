0ca496104c2275f3177f41968687a8fd4d4cca38
jurin-n <jurin.nakano@gmail.com>
2019-04-03 23:46:57 +0900

    fix #1

---
+++ web-demo/src/main/java/com/jurinn/web/demo/MenuItem.java
@@ -0,0 +1,28 @@
+package com.jurinn.web.demo;
+
+public class MenuItem {
+    private String dispName;
+    private String path;
+
+    public MenuItem(String dispName, String path) {
+        super();
+        this.dispName = dispName;
+        this.path = path;
+    }
+
+    public String getDispName() {
+        return dispName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setDispName(String dispName) {
+        this.dispName = dispName;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- web-demo/src/main/java/com/jurinn/web/demo/TopController.java
+++ web-demo/src/main/java/com/jurinn/web/demo/TopController.java
@@ -1,5 +1,6 @@
 package com.jurinn.web.demo;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,13 @@ public class TopController {
     @GetMapping
     String top(Model model) {
         List<TopInfo> topInfo = topService.getTopInfoList();
+        List<MenuItem> menuItems = Arrays.asList(
+                new MenuItem("メニュー０１","/addInfo"),
+                new MenuItem("メニュー０２","/deleteInfo"),
+                new MenuItem("メニュー０３","/list"),
+                new MenuItem("メニュー０４","/search")
+                );
+        model.addAttribute("menuItems", menuItems);
         model.addAttribute("topInfo", topInfo);
         return "top/index";
     }
