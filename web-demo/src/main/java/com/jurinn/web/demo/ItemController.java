2f979124f805e9c2ef5d82eb7818438d118590ff
jurin-n <jurin.nakano@gmail.com>
2019-04-11 22:00:18 +0900

    一覧画面に詳細ボタン追加。編集画面追加(一部未実装)。

--- web-demo/src/main/java/com/jurinn/web/demo/ItemController.java
+++ web-demo/src/main/java/com/jurinn/web/demo/ItemController.java
@@ -3,6 +3,7 @@ package com.jurinn.web.demo;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/items")
@@ -28,20 +30,31 @@ public class ItemController {
     }
 
     @GetMapping
-    String getItems(Model model) {
+    String getItems(@RequestParam(name = "id", required = false) Optional<String> itemId, Model model) {
         List<MenuItem> menuItems = menuService.getMenuItems();
         model.addAttribute("menuItems", menuItems);
+
+        if (itemId.isPresent()) {
+            return getItemEditPage(itemId.get(), model);
+        }
+
         model.addAttribute("items", itemService.getItems());
         return "item/list";
     }
-    
+
+    private String getItemEditPage(String itemId, Model model) {
+        //TODO:DBから商品とるロジック実装。
+        System.out.println("itemId=" + itemId);
+        return "item/edit";
+    }
+
     @GetMapping("/add")
     String getItemAddPage(Model model) {
         List<MenuItem> menuItems = menuService.getMenuItems();
         model.addAttribute("menuItems", menuItems);
         return "item/add";
     }
- 
+
     @PostMapping("/add")
     String addItem(@Validated ItemForm form, BindingResult result, Model model) {
         if (result.hasErrors()) {
@@ -49,9 +62,9 @@ public class ItemController {
         }
         // TODO: BeanUtilsまたは、Dozer、ModelMapper検討。はじめてのSpring Boot p100 参照。
         LocalDateTime now = LocalDateTime.now(ZoneId.of("GMT"));
-        Item item = new Item(form.getItemId(),form.getName(),form.getDescription(),now);
-        Price price = new Price(form.getActivateFrom(),form.getActivateTo(),form.getPrice(),now);
-        
+        Item item = new Item(form.getItemId(), form.getName(), form.getDescription(), now);
+        Price price = new Price(form.getActivateFrom(), form.getActivateTo(), form.getPrice(), now);
+
         itemService.add(item, price);
         return "redirect:/items/add";
     }
